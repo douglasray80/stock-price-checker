73ac9017300b29528f369647d9375e4a5213b102
Douglas Ray <dpr980@gmail.com>
2019-05-31 20:28:38 +0100

    implemented logic for get and like single stock

--- routes/api.js
+++ routes/api.js
@@ -13,14 +13,20 @@ module.exports = function(app) {
 	app.route('/api/stock-prices').get(async (req, res, next) => {
 		const stock_symbol = req.query.stock;
 		const like = req.query.like;
-		const ipAddress = req.ip;
+		const user_ip = req.ip;
 
 		try {
-			const stock = await StockService.getStock(stock_symbol);
+			let stock = await StockService.getStock(stock_symbol);
 
-			if (like) await StockService.likeStock(stock_symbol, ipAddress);
+			if (like) stock = await StockService.likeStock(stock._id, user_ip);
 
-			res.json({ stockData: stock });
+			res.json({
+				stockData: {
+					stock: stock.symbol,
+					price: stock.price,
+					likes: stock.likes.length
+				}
+			});
 		} catch (err) {
 			next(err);
 		}
